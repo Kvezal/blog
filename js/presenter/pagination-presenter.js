42e64c728081e1d70bd3add3ea7b52f0498ed455
Maxim Voropai <muckam49@mail.ru>
2017-12-05 16:03:13 +0800

    component of the pagination created

--- js/presenter/pagination-presenter.js
+++ js/presenter/pagination-presenter.js
@@ -1,11 +1,39 @@
+import App from '../application';
 import PaginationView from '../views/pagination-view';
 import Utils from '../lib/utils';
 
 class PaginationPresenter {
-  init(parameters) {
+  init(parameters, oldElement) {
     this.view = new PaginationView(parameters);
 
-    Utils.replaceOldElement(this.view.element, parameters.oldElement);
+    this.view.showPage = (evt) => {
+      evt.preventDefault();
+
+      const state = this.view.parameters.state;
+      state.currentPage = +evt.target.textContent;
+
+      App.changeTab(state);
+    };
+
+    this.view.showPreviousPage = (evt) => {
+      evt.preventDefault();
+
+      const state = this.view.parameters.state;
+      --state.currentPage;
+
+      App.changeTab(state);
+    };
+
+    this.view.showNextPage = (evt) => {
+      evt.preventDefault();
+
+      const state = this.view.parameters.state;
+      ++state.currentPage;
+
+      App.changeTab(state);
+    };
+
+    Utils.replaceOldElement(this.view.element, oldElement);
   }
 }
 
--- js/views/pagination-view.js
+++ js/views/pagination-view.js
@@ -1,27 +1,89 @@
 import AbstractView from './abstract-view';
 
+const FIRST_PAGE = 1;
+const SPREADING = 2;
+const STEP = 1;
+const PLACEHOLDER = `<span class="pagination__placeholder"> ... </span>`;
+
 class PaginationView extends AbstractView {
   constructor(parameters) {
     super();
-
-    this.amountDataItems = parameters.amountDataItems;
-    this.currentPage = parameters.currentPage;
-    this.maxAmountItemsOnPage = parameters.maxAmountItemsOnPage;
+    this.parameters = parameters;
   }
 
   get template() {
-    const amountOfPage = Math.ceil(this.amountDataItems / this.maxAmountItemsOnPage);
+    return (
+      `<div class="pagination">
+        ${this.templateItems}
+      </div>`
+    );
+  }
 
-    const pageList = [`<div class="pagination"><a class="pagination__item  pagination__item--prev  pagination__item--disabled">Назад</a>`];
-    for (let i = 1; i <= amountOfPage; i++) {
-      if (i === this.currentPage) {
+  get templateItems() {
+    const amountOfPage = Math.ceil(this.parameters.amountDataItems / this.parameters.maxAmountItemsOnPage);
+
+    if (!amountOfPage) {
+      return ``;
+    }
+
+    let startItem = this.parameters.state.currentPage - SPREADING;
+    startItem = (startItem > FIRST_PAGE) ? startItem : FIRST_PAGE;
+
+    let endItem = this.parameters.state.currentPage + SPREADING;
+    endItem = (endItem < amountOfPage) ? endItem : amountOfPage;
+
+    const pageList = [];
+
+    const previous = (this.parameters.state.currentPage === FIRST_PAGE) ?
+      `<a class="pagination__item  pagination__item--prev  pagination__item--disabled">Назад</a>` :
+      `<a class="pagination__item  pagination__item--prev" href="#">Назад</a>`;
+    pageList.push(previous);
+
+    if (startItem > FIRST_PAGE) {
+      const nextElementOfPagination = FIRST_PAGE + STEP;
+      const paginationPlaceholder = (nextElementOfPagination === startItem) ?
+        `<a class="pagination__item" href="#">${FIRST_PAGE}</a>` :
+        `<a class="pagination__item" href="#">${FIRST_PAGE}</a> ${PLACEHOLDER}`;
+      pageList.push(paginationPlaceholder);
+    }
+
+    for (let i = startItem; i <= endItem; i++) {
+      if (i === this.parameters.state.currentPage) {
         pageList.push(`<a class="pagination__item  pagination__item--current">${i}</a>`);
         continue;
       }
       pageList.push(`<a class="pagination__item" href="#">${i}</a>`);
     }
-    pageList.push(`<a class="pagination__item  pagination__item--next" href="#">Вперед</a></div>`);
+
+    if (endItem < amountOfPage) {
+      const previousElementOfPagination = amountOfPage - STEP;
+      const paginationPlaceholder = (previousElementOfPagination === endItem) ?
+        `<a class="pagination__item" href="#">${amountOfPage}</a>` :
+        `${PLACEHOLDER} <a class="pagination__item" href="#">${amountOfPage}</a>`;
+      pageList.push(paginationPlaceholder);
+    }
+
+    const next = (this.parameters.state.currentPage === amountOfPage) ?
+      `<a class="pagination__item  pagination__item--next  pagination__item--disabled">Вперед</a>` :
+      `<a class="pagination__item  pagination__item--next" href="#">Вперед</a>`;
+    pageList.push(next);
+
     return pageList.join(``);
+  }
+
+  bind(element) {
+    const pages = element.querySelectorAll(`.pagination__item:not(.pagination__item--prev):not(.pagination__item--next):not(.pagination__item--current)`);
+    pages.forEach((page) => page.addEventListener(`click`, this.showPage));
+
+    const previous = element.querySelector(`.pagination__item--prev:not(.pagination__item--disabled)`);
+    if (previous) {
+      previous.addEventListener(`click`, this.showPreviousPage);
+    }
+
+    const next = element.querySelector(`.pagination__item--next:not(.pagination__item--disabled)`);
+    if (next) {
+      next.addEventListener(`click`, this.showNextPage);
+    }
   }
 }
 
